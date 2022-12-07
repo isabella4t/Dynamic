031bc1e1f1cc9ec874eb9759d1b562465c072c5b
Izzy T <itang2023@kehillahstudent.org>
2022-12-07 14:37:57 -0800

    Addded factory method

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,41 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
+
+        //fib
         for(int i=0; i<50;i++){
             System.out.println(fib(i));
             System.out.println(ne(fib(i)));
         }
 
+        System.out.println("Factory Problem");
+        System.out.println(factory(7));
+
     }
 
     //tail recursion can be shoved into nonrecursive code (a loop)
     static  HashMap<Integer,Integer> fibs = new HashMap<Integer, Integer>();
+
+    static int fib(int n){
+        if(n<2) return 1;
+        if(fibs.containsKey(n)) return fibs.get(n);
+        int nm1 = fib(n-1);
+        int nm2 = fib(n-2);
+        fibs.put(n,nm1+nm2);
+        //return nm1 + nm2;
+        return nm1 + nm2;
+    }
+
+    static  HashMap<Integer,Boolean> fac = new HashMap<Integer, Boolean>();
+
+   static boolean factory(int n){
+       if(n==0) return false;
+       if(!fac.containsKey(n-1)) return true;
+       boolean before = factory(n-1);
+       fac.put(n,!before);
+       //return nm1 + nm2;
+       return !before;
+   }
 
 
     //ne is the warm up
@@ -28,27 +54,9 @@ public class Main {
         - if P(0) and (if P(n) then P(n+1) it proves the next one)
         then
         p for any integer is true
-
+prove stuff
          */
     }
-
-    //prove
-
-    static int fib(int n){
-        if(n<2) return 1;
-        if(fibs.containsKey(n)) return fibs.get(n);
-        int nm1 = fib(n-1);
-        int nm2 = fib(n-2);
-        fibs.put(n,nm1+nm2);
-        //return nm1 + nm2;
-        return nm1 + nm2;
-    }
-
-    static  HashMap<Integer,Integer> factories = new HashMap<Integer, Integer>();
-
-   // static int factory(int n){}
-
-
 
 
 }
